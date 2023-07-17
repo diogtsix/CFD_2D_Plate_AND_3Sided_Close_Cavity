0394b4fbdba320a6343dc3c09f6b1b621e8a0dc5
diogtsix <diogtsix@gmail.com>
2023-07-17 22:39:30 +0200

    soolver initialization

---
+++ solver.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Solver: 
+    
+    def __init__(self):
+        pass
+    
+    def solver_explicit(self):
+        pass
+    
+    
+    def solver_implicit(self):
+        pass
+    
+    def results(self):
+        pass
+    
