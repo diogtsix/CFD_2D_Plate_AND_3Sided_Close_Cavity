3ff83d0f3e2eefcb9d780901077c7d28f55ba39f
diogtsix <diogtsix@gmail.com>
2023-07-17 22:54:32 +0200

    WIP

--- main.py
+++
@@ -1,6 +0,0 @@
-
-# class structure : Preprocessor - Solver - PostProcessor 
-
-# Preprocessor : Node id, Position, initial vel etc.
-# solver numerical methods, differences 
-#Post processor : Visualization 
