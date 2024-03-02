1ec53314b4fc8ef9c453ae67de3e2f6c335c7ec0
diogtsix <diogtsix@gmail.com>
2024-03-02 16:27:45 +0200

    bulding classes starting with preprocessor and removed the dataobj folder

---
+++ preprocessor/Cavity_preprocessor.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+class cavity_preprocessor:
+    
+    
+    def __init__(self, wall_length, dx, dy, relax_factor, wall_vel, 
+                 reynolds, error):
+        
+        
+        self.wall_length = wall_length
+        self.dx = dx
+        self.dy = dy
+        self.relax_factor = relax_factor
+        self.wall_vel = wall_vel
+        self.reynolds = reynolds
+        self.error = error
+        
+        self.nodx = None 
+        self.nody = None 
+        self.xCoords = None
+        self.yCoords = None
+        self.u_vel = None
+        self.v_vel = None
+        self.psi1 = None
+        self.psi2 = None
+        self.psi3 = None
+        self.zhta1 = None
+        self.zhta2 = None
+        self.zhta3 = None
+        
+        
+    def create_grid(self):
+        
+        # Initialize matrices and constants
+        self.nodx = round(self.wall_length / self.dx) + 1
+        self.nody = round(self.wall_length / self.dy) + 1
+        
+        self.xCoords = np.linspace(0, self.wall_length  ,self.nodx)
+        self.yCoords = np.linspace(0, self.wall_length  ,self.nody)
+        
+        self.u_vel = np.zeros(self.nody, self.nodx)
+        self.v_vel = np.zeros(self.nody, self.nodx)
+        self.psi1 = np.zeros(self.nody, self.nodx)
+        self.psi2 = np.zeros(self.nody, self.nodx)
+        self.psi3 = np.zeros(self.nody, self.nodx)
+        self.zhta1 = np.zeros(self.nody, self.nodx)
+        self.zhta2 = np.zeros(self.nody, self.nodx)
+        self.zhta3 = np.zeros(self.nody, self.nodx)
+    
+    
+    def initialize_velocity_field(self):
+        
+        self.create_grid()   
+        # Set top wall velocity 
+        self.u_vel[-1,:] = self.wall_vel
+        
